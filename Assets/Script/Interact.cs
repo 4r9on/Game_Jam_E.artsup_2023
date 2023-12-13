7550d16c00060dac44787e172f7a92da0d489ce9
titouan garnier <garniertitouan4@gmail.com>
2023-12-13 14:13:39 +0100

    Item/Info

---
+++ Assets/Script/Interact.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Interact : MonoBehaviour
+{
+    public GameObject text;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            text.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            text.SetActive(false);
+        }
+    }
+}
